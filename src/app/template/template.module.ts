91ab1ebc76d8c400345560c66e0b55b27e2ae5cb
Luis Morales <dev.luismorales@gmail.com>
2022-06-13 22:13:41 -0500

    se cargo por lazi load los modulos creados

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TemplateRoutingModule } from './template-routing.module';
+import { DinamicosComponent } from './dinamicos/dinamicos.component';
+import { BasicosComponent } from './basicos/basicos.component';
+import { SwitchesComponent } from './switches/switches.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DinamicosComponent,
+    BasicosComponent,
+    SwitchesComponent
+  ],
   imports: [
     CommonModule,
     TemplateRoutingModule
