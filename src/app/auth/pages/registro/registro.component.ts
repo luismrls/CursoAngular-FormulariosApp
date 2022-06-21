413a4c2f14daf787141713f3dad3edc3dffa4890
Luis Morales <dev.luismorales@gmail.com>
2022-06-20 22:15:17 -0500

    se hizo la validacion para el compo de email

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -9,16 +9,22 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class RegistroComponent implements OnInit {
 
-  nombreApellidoPatter: string = '([a-zA-Z]+) ([a-zA-Z]+)'
+  nombreApellidoPatter: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
   miFormulario: FormGroup = this.formBuilder.group({
     nombre: ['', [Validators.required, Validators.pattern( this.nombreApellidoPatter ) ] ],
-
+    email: ['', [Validators.required,  Validators.pattern( this.emailPattern ) ] ],
   })
 
   constructor( private formBuilder: FormBuilder ) { }
 
   ngOnInit(): void {
+
+    this.miFormulario.reset({
+      nombre: "Luis Morales",
+      email: 'test@test.com'
+    });
   }
 
   campoNoValido( campo: string ) {
