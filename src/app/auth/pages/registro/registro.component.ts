370590dff7dac5f99cb30f1c3879706495fa9fe8
Luis Morales <dev.luismorales@gmail.com>
2022-06-21 07:59:10 -0500

    haciendo validaciones personalizadas para el input de username

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -12,9 +12,21 @@ export class RegistroComponent implements OnInit {
   nombreApellidoPatter: string = '([a-zA-Z]+) ([a-zA-Z]+)';
   emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
+  noPuedeSerStrider( control: FormControl ) {
+    const valor: string = control.value?.trim().toLowerCase();
+    if( valor === 'strider' ){
+      return {
+        noStrider: true
+      }
+    }
+
+    return null;
+  }
+
   miFormulario: FormGroup = this.formBuilder.group({
     nombre: ['', [Validators.required, Validators.pattern( this.nombreApellidoPatter ) ] ],
     email: ['', [Validators.required,  Validators.pattern( this.emailPattern ) ] ],
+    username: ['', [Validators.required, this.noPuedeSerStrider ] ],
   })
 
   constructor( private formBuilder: FormBuilder ) { }
